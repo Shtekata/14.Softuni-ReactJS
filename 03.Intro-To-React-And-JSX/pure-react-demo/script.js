08b3f808e52d16386bb0a8ebe7a4de39fa46de30
Shtekata <gesheval@gmail.com>
2021-03-06 13:12:01 +0200

    Add JSX variable

--- 03.Intro-To-React-And-JSX/pure-react-demo/script.js
+++ 03.Intro-To-React-And-JSX/pure-react-demo/script.js
@@ -8,6 +8,21 @@ const reactElement = React.createElement(
     React.createElement('h3', {}, 'Ala-Bala')
 );
 
-console.log(reactElement);
+// console.log(reactElement);
 
-ReactDOM.render(reactElement, domElement);
+// const reactJsxElement = <header>
+//     <h1>Hello From React JSX Element</h1>
+//     <h2>The Best Framework Ever</h2>
+//     <h3>Ala-Bala</h3>
+// </header>
+
+const reactJsxElement = (
+    <header>
+        <h1>Hello From React JSX Element</h1>
+        <h2>The Best Framework Ever</h2>
+        <h3>Ala-Bala</h3>
+    </header>
+);
+
+// ReactDOM.render(reactElement, domElement);
+ReactDOM.render(reactJsxElement, domElement);
--- server/public/script.js
+++ server/public/script.js
@@ -10,4 +10,19 @@ const reactElement = React.createElement(
 
 // console.log(reactElement);
 
-ReactDOM.render(reactElement, domElement);
+// const reactJsxElement = <header>
+//     <h1>Hello From React JSX Element</h1>
+//     <h2>The Best Framework Ever</h2>
+//     <h3>Ala-Bala</h3>
+// </header>
+
+const reactJsxElement = (
+    <header>
+        <h1>Hello From React JSX Element</h1>
+        <h2>The Best Framework Ever</h2>
+        <h3>Ala-Bala</h3>
+    </header>
+);
+
+// ReactDOM.render(reactElement, domElement);
+ReactDOM.render(reactJsxElement, domElement);
