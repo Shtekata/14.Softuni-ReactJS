760fcd40c656505458ce883a0692d503777a5a66
Shtekata <gesheval@gmail.com>
2021-03-06 12:43:39 +0200

    Pure React Demo and Server with React

---
+++ server/public/script.js
@@ -0,0 +1,13 @@
+const domElement = document.getElementById('root');
+
+const reactElement = React.createElement(
+    'header',
+    {},
+    React.createElement('h1', {}, 'Hello from React Element'),
+    React.createElement('h2', {}, 'The Best Framework Ever'),
+    React.createElement('h3', {}, 'Ala-Bala')
+);
+
+// console.log(reactElement);
+
+ReactDOM.render(reactElement, domElement);
